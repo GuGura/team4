e0ecaa50ce17f461f937a800cac4bae94fd5b6a7
DaegilTheRuler <tjsdwngp@gmail.com>
2023-06-21 16:39:37 +0900

    ㅇㅇ

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-import router from "../script/routes/router";
-import {createPinia} from "pinia";
-
-
+import store from "../script/store";
+import router from "../script/router";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
 createApp(App)
-    .use(createPinia())
+    .use(store)
     .use(router)
     .mount('#app')
