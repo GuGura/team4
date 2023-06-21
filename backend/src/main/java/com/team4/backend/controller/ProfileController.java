22055e73efee05c84733db2982399b5cb76e8b2b
DaegilTheRuler <tjsdwngp@gmail.com>
2023-06-21 21:39:57 +0900

    ㅇㅇ

--- backend/src/main/java/com/team4/backend/controller/ProfileController.java
+++ backend/src/main/java/com/team4/backend/controller/ProfileController.java
@@ -1,16 +1,25 @@
 package com.team4.backend.controller;
 
-import com.team4.backend.model.dto.EventDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.team4.backend.model.dto.UserDetailDTO;
+import com.team4.backend.service.ProfileService;
+import com.team4.backend.util.UserUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
 import java.util.UUID;
 
 @RestController
 public class ProfileController {
+
+    @Autowired
+    public ProfileController(ProfileService profileService){this.profileService = profileService;}
+
+    private final ProfileService profileService;
 
     @PostMapping("api/v1/home/changeProfileImage")
     public String saveProfileImage(MultipartFile[] uploadFile){
@@ -36,5 +45,29 @@ public class ProfileController {
             }
         }
         return uploadFileName;
+    }
+
+   @PutMapping("api/v1/home/updateProfile")
+    public void updateProfile(@RequestBody UserDetailDTO userDetail){
+        String email = UserUtil.getEmail();
+        userDetail.setUserName(email);
+        profileService.updateProfile(userDetail);
+    }
+
+    //@PostMapping("api/v1/home/updateProfile")
+    public void createProfile(@RequestBody UserDetailDTO userDetail){
+        String email = UserUtil.getEmail();
+        userDetail.setUserName(email);
+        profileService.createProfile(userDetail);
+    }
+
+    @PostMapping("api/v1/home/updateIcon")
+    public void updateIcon(@RequestBody File uploadFile){
+        String uploadFolder = "C:\\upload\\images\\users\\"+UserUtil.getEmail().substring(0,UserUtil.getEmail().lastIndexOf("@"))+"\\icons";
+        File file = new File(uploadFolder);
+        if (file.exists() == false) {
+            file.mkdirs();
+        }String uploadFileName = null;
+        File uploadingFile = new File(file, uploadFile);
     }
 }
