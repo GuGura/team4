e3073aa82f801f0ba0ce76da9ea8caca080b420d
gumaggo <lauv.0309@gmail.com>
2023-06-23 21:30:00 -0700

    야호

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,13 @@ import store from "../script/store";
 import router from "../script/router";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { createPinia } from 'pinia'
 
-
+const pinia = createPinia()
 createApp(App)
     .use(store)
     .use(router)
+    .use(pinia)
     .mount('#app')
+
+
