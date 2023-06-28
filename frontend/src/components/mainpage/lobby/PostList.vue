1e4e48c694892e991f4497efb7f1cde28fbe8273
gumaggo <lauv.0309@gmail.com>
2023-06-28 15:47:10 -0700

    야호

---
+++ frontend/src/components/mainpage/lobby/PostList.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action">
+            <div class="content-wrapper">
+                <div class="content">
+                    <h5 class="mb-1">작성자</h5>
+                    <div class="mb-1">작성 날짜</div>
+                    <h4 class="mb-1">게시물 제목</h4>
+                    <div class="mb-1">게시물 내용</div>
+                </div>
+                <div class="image">
+                    <img src="대충 이미지 넣기" alt="Post Image">
+                </div>
+            </div>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            <div class="content-wrapper">
+                <div class="content">
+                    <h5 class="mb-1">작성자</h5>
+                    <div class="mb-1">작성 날짜</div>
+                    <h4 class="mb-1">게시물 제목</h4>
+                    <div class="mb-1">게시물 내용</div>
+                </div>
+                <div class="image">
+                    <img src="대충 이미지 넣기2" alt="Post Image">
+                </div>
+            </div>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            <div class="content-wrapper">
+                <div class="content">
+                    <h5 class="mb-1">작성자</h5>
+                    <div class="mb-1">작성 날짜</div>
+                    <h4 class="mb-1">게시물 제목</h4>
+                    <div class="mb-1">게시물 내용</div>
+                </div>
+                <div class="image">
+                    <img src="대충 이미지 넣기3" alt="Post Image">
+                </div>
+            </div>
+        </a>
+        <div class="row my-2 mx-auto">
+            <button type="button" class="btn btn-sm btn-primary" id="boardMoreButton" @click="boardPaging()">더 보기 ({{pagingInfo}})</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostList",
+};
+</script>
+
+<style>
+.content-wrapper {
+    display: flex;
+    align-items: center;
+}
+
+.content {
+    flex: 1;
+}
+
+.image {
+    margin-left: 20px;
+}
+
+img {
+    max-width: 100%;
+}
+</style>
---
+++ frontend/src/components/mainpage/lobby/Tab.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="tab-container">
+        <div class="tab-buttons">
+            <button v-for="(tab, index) in tabs" :key="index" :class="{'tab-button': true, 'active': activeTab === index}" @click="changeTab(index)">
+                {{ tab }}
+            </button>
+        </div>
+        <div class="tab-content">
+            <div v-for="(content, index) in tabContents" :key="index" :class="{'tab-pane': true, 'active': activeTab === index}">
+                <div v-if="index === 0">
+                    <div class="content-box">
+                        <Profile/>
+                    </div>
+                </div>
+                <div v-else-if="index === 1">
+                    <div class="content-box">
+                        <Calendar :key="calendarKey" />
+                    </div>
+                </div>
+                <div v-else-if="index === 2">
+                    <div class="content-box">
+                        <RichTextEditor/>
+                    </div>
+                </div>
+                <div v-else-if="index === 3">
+                    <div class="content-box">
+                        <PostList/>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Profile from "@/components/mainpage/lobby/Profile.vue";
+import RichTextEditor from "@/components/mainpage/lobby/RichTextEditor.vue";
+import Calendar from "@/components/mainpage/lobby/Calendar.vue";
+import PostList from "@/components/mainpage/lobby/PostList.vue";
+
+export default {
+    components: {PostList, Calendar, RichTextEditor, Profile },
+    data() {
+        return {
+            tabs: ['프로필 수정', '일정 보기', '글 작성', '글 목록'],
+            tabContents: [
+                'Content for Tab 1',
+                'Content for Tab 2',
+                null, // Placeholder for the RichTextEditor component
+                'Content for Tab 4'
+            ],
+            activeTab: 0,
+        };
+    },
+    methods: {
+        changeTab(index) {
+            this.activeTab = index;
+        },
+    },
+};
+</script>
+
+<style scoped>
+.content-box {
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    border: 1px solid black;
+    padding: 20px;
+}
+
+.tab-container {
+    width: 100%;
+}
+
+.tab-buttons {
+    display: flex;
+}
+
+.tab-button {
+    width: 100%;
+    padding: 10px 20px;
+    background-color: lightgray;
+    border: none;
+    outline: none;
+    cursor: pointer;
+}
+
+.tab-button.active {
+    background-color: gray;
+    color: white;
+}
+
+.tab-pane {
+    display: none;
+}
+
+.tab-pane.active {
+    display: block;
+}
+</style>
