4f0736c83d5da8ba551ec401057873fc10149679
King <wodus331@gmail.com>
2023-06-29 20:18:18 +0900

    방생성하는 모달창

--- frontend/src/components/modal/CreateRoomModal.vue
+++ frontend/src/components/modal/CreateRoomModal.vue
@@ -11,13 +11,26 @@ function closeModal() {
 <template>
   <div id="background" @click="closeModal"></div>
   <div id="modal">
-
+    <div style="display: flex;align-items: center;justify-content: space-between">
+      <div style="color: #fff;font-size: 21px">채널 만들기</div>
+      <div id="exit" @click="closeModal">
+        <img src="/img/serverlist/exit.png">
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-#modal{
-  width: 500px;
+#exit{
+  height:16px;
+  cursor: pointer;
+  filter: opacity(0.5);
+}
+#exit:hover{
+  filter: opacity(1);
+}
+#modal {
+  width: 430px;
   height: 300px;
   background-color: #313338;
   z-index: 13;
@@ -26,7 +39,11 @@ function closeModal() {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  padding: 20px 15px;
 }
+
 #background {
   background-color: black;
   filter: opacity(0.5);
@@ -36,5 +53,9 @@ function closeModal() {
   left: 0;
   position: fixed;
   z-index: 12;
+}
+img{
+  width: 100%;
+  height: 100%;
 }
 </style>
