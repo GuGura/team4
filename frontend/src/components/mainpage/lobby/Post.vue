3b670608b042bf73b778761868d45ec674a46f1d
gumaggo <lauv.0309@gmail.com>
2023-06-29 18:13:16 -0700

    글 목록 수정

--- frontend/src/components/mainpage/lobby/Post.vue
+++ frontend/src/components/mainpage/lobby/Post.vue
@@ -10,22 +10,22 @@ export default {
 </script>
 
 <template>
-    <a href="#" class="list-group-item list-group-item-action">
-        <div class="content-wrapper">
-            <div class="content">
-                <h5 class="mb-1">{{post.username}}</h5>
-                <div class="mb-1">{{post.uploadDate}}</div>
-                <h4 class="mb-1">{{post.title}}</h4>
-                <div class="mb-1">{{post.content}}</div>
-            </div>
-            <div class="image">
-                <img alt="Post Image" :src="'data:image/png;base64,'+post.contentIMG">
-            </div>
+    <div class="card mb-3">
+        <img src="/img/sidebar/DM_icon.png" class="card-img-top" alt="Post Image">
+        <div class="card-body">
+            <h5 class="card-title">{{ post.username }}</h5>
+            <h4 class="card-subtitle mb-2">{{ post.title }}</h4>
+            <p class="card-text">{{ post.content }}</p>
+            <p class="card-text"><small class="text-body-secondary">Last updated {{ post.uploadDate }}</small></p>
         </div>
-    </a>
+    </div>
 </template>
 
 <style scoped>
 .content{ text-overflow:ellipsis; white-space: nowrap; overflow: hidden;-webkit-box-orient: vertical; -webkit-line-clamp: 2;}
-
+.card-img-top {
+    width: 100%;
+    height: 15vw;
+    object-fit: cover;
+}
 </style>
