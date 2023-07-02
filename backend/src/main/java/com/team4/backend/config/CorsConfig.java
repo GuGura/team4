b19bea7dae4cf1f3d996dc13a5e353af0957c424
King <wodus331@gmail.com>
2023-07-02 23:38:08 +0900

    배포전 테스트

--- backend/src/main/java/com/team4/backend/config/CorsConfig.java
+++ backend/src/main/java/com/team4/backend/config/CorsConfig.java
@@ -15,15 +15,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://www.meatteam.online", "http://meatteam.online","http://api.meatteam.online")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .exposedHeaders("Access-Control-Allow-Origin", JwtProperties.HEADER_ACCESS, JwtProperties.HEADER_REFRESH);
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
+        config.addAllowedHeader("*"); // 모든 header에 응답을 허용하겠다.
+        config.addAllowedMethod("*"); // 모든 post,get,put,delete,patch 요청을 허용하겠다.
+        config.addAllowedOriginPattern("http://www.meatteam.online");
+        config.addExposedHeader("Access-Control-Allow-Origin");
+        config.addExposedHeader(JwtProperties.HEADER_ACCESS);
+        config.addExposedHeader(JwtProperties.HEADER_REFRESH);
+        source.registerCorsConfiguration("/api/**",config);
+        source.registerCorsConfiguration("/login",config);
+        source.registerCorsConfiguration("/**",config);
+        source.registerCorsConfiguration("/ws/**",config);
+        return new CorsFilter(source);
     }
 
 }
